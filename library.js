a84454b42e409c3afbfbdf83625f0b46ca6464cf
Joseph McCann <joe.mccann@mailbox.org>
2022-09-30 18:29:15 -0400

    Implement rudimentary 'remove book' funcationality.

--- library.js
+++ library.js
@@ -1,5 +1,7 @@
 
-const myLibrary = [];
+const myLibrary = [new Book('hey you', 'author mcgee', '123', true)];
+
+displayBooks();
 
 function Book(
   title,
@@ -13,46 +15,57 @@ function Book(
   this.read = read
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book)
-  return book;
+function addBookToLibrary(event) {
+  event.preventDefault();
+  const title = document.querySelector('#title').value
+  const author = document.querySelector('#author').value
+  const pageCount = document.querySelector('#page_count').value
+  const isRead = document.querySelector('#is_read').checked
+  
+  const book = new Book(
+    title,
+    author,
+    pageCount,
+    isRead
+  )
+  myLibrary.push(book);
+  displayBooks();
 }
 
-const book1 = new Book(
-  'Lord of the Rings',
-  'J.R.R. Tolkien',
-  '500',
-  true
-)
-
-const book2 = new Book(
-  'Harry Potter',
-  'J.K. Rowling',
-  '400',
-)
-
-addBookToLibrary(book1);
-addBookToLibrary(book2)
 
 function displayBooks() {
   const list = document.querySelector('#library');
-  myLibrary.forEach((book) => {
+  list.replaceChildren();
+  myLibrary.forEach((book, index) => {
     const listItem = document.createElement('li');
     [book.title, book.author, book.pageCount, book.read].forEach((attr) => {
       const attribute = document.createElement('p');
       attribute.textContent = attr
       listItem.appendChild(attribute);
     });
+    listItem.dataset.bookId = index
+    const removeButton = document.createElement('button');
+    removeButton.setAttribute('id', `remove-${listItem.dataset.bookId}`)
+    removeButton.textContent = 'remove book'
+    listItem.appendChild(removeButton)
     list.appendChild(listItem);
+    removeButton.addEventListener('click', () => {
+      list.removeChild(listItem)
+      myLibrary.splice(index, 1)
+    });
   });
 }
-
-const addBookButton = document.querySelector('#add_book_button');
-addBookButton.addEventListener('click', showBookForm);
 
 function showBookForm() {
   const form = document.querySelector('.form');
   form.classList.toggle('hidden');
 }
 
-displayBooks();
+const addBookButton = document.querySelector('#add_book_button');
+addBookButton.addEventListener('click', showBookForm);
+
+const addBookForm = document.querySelector('#add_book_form');
+addBookForm.addEventListener('submit', addBookToLibrary)
+
+
+
