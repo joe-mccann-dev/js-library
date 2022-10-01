e2a0ffb45339d53c8b49bed0d838f6b8bd99d831
Joseph McCann <joe.mccann@mailbox.org>
2022-10-01 02:36:26 -0400

    Add mark as read feature. General refactor.

--- library.js
+++ library.js
@@ -1,13 +1,9 @@
 
-const myLibrary = [new Book('hey you', 'author mcgee', '123', true)];
-
-displayBooks();
-
 function Book(
   title,
   author,
   pageCount,
-  read = false
+  read = 'unread'
 ) {
   this.title = title;
   this.author = author;
@@ -15,57 +11,92 @@ function Book(
   this.read = read
 }
 
-function addBookToLibrary(event) {
-  event.preventDefault();
-  const title = document.querySelector('#title').value
-  const author = document.querySelector('#author').value
-  const pageCount = document.querySelector('#page_count').value
-  const isRead = document.querySelector('#is_read').checked
-  
-  const book = new Book(
-    title,
-    author,
-    pageCount,
-    isRead
-  )
-  myLibrary.push(book);
-  displayBooks();
+Book.prototype.toggleRead = function () {
+  this.read = this.read === 'read' ? 'unread' : 'read'
 }
 
-
-function displayBooks() {
-  const list = document.querySelector('#library');
-  list.replaceChildren();
-  myLibrary.forEach((book, index) => {
-    const listItem = document.createElement('li');
-    [book.title, book.author, book.pageCount, book.read].forEach((attr) => {
-      const attribute = document.createElement('p');
-      attribute.textContent = attr
-      listItem.appendChild(attribute);
-    });
-    listItem.dataset.bookId = index
-    const removeButton = document.createElement('button');
-    removeButton.setAttribute('id', `remove-${listItem.dataset.bookId}`)
-    removeButton.textContent = 'remove book'
-    listItem.appendChild(removeButton)
-    list.appendChild(listItem);
-    removeButton.addEventListener('click', () => {
-      list.removeChild(listItem)
-      myLibrary.splice(index, 1)
-    });
-  });
-}
+const addBookButton = document.querySelector('#add_book_button');
+addBookButton.addEventListener('click', showBookForm);
 
 function showBookForm() {
   const form = document.querySelector('.form');
   form.classList.toggle('hidden');
 }
 
-const addBookButton = document.querySelector('#add_book_button');
-addBookButton.addEventListener('click', showBookForm);
-
 const addBookForm = document.querySelector('#add_book_form');
 addBookForm.addEventListener('submit', addBookToLibrary)
 
+let library = [];
+function addBookToLibrary(e) {
+  e.preventDefault();
 
+  const title = document.querySelector('#title').value
+  const author = document.querySelector('#author').value
+  const pageCount = document.querySelector('#page_count').value
+  const isRead = document.querySelector('#is_read').checked ? 'read' : 'unread'
 
+  const book = new Book(
+    title,
+    author,
+    pageCount,
+    isRead
+  )
+  // start with a fresh array instead of tacking onto long list of undefined
+  // this way, array always represents actual state of library
+  if (library.every((book) => book === undefined)) { library = [];}
+  library.push(book)
+  displayBook(book, library.length - 1)
+}
+
+const list = document.querySelector('#library');
+function displayBook(book, index) {
+  const listItem = document.createElement('li');
+
+  [
+    book.title,
+    book.author,
+    book.pageCount,
+    book.read
+  ].forEach((attr) => {
+    const element = document.createElement('p');
+    if (attr === book.read) { element.setAttribute('id', `read-${index}`) }
+    element.textContent = attr
+    listItem.appendChild(element);
+  });
+
+  listItem.dataset.bookId = index
+
+  const removeButton = document.createElement('button');
+  removeButton.setAttribute('class', 'remove_button');
+  removeButton.setAttribute('id', `remove-${listItem.dataset.bookId}`);
+  removeButton.textContent = 'remove book'
+  listItem.appendChild(removeButton);
+
+  const toggleReadButton = document.createElement('button');
+  toggleReadButton.setAttribute('class', 'toggle_read_button');
+  toggleReadButton.setAttribute('id', `mark-${listItem.dataset.bookId}`);
+  toggleReadButton.textContent = 'toggle read'
+  listItem.appendChild(toggleReadButton);
+
+  list.appendChild(listItem);
+}
+
+list.addEventListener('click', (e) => {
+  const target = e.target
+
+  if (target.className === 'remove_button') {
+    const listItem = target.parentElement;
+    const bookId = target.id.split('-')[1];
+    delete library[bookId]
+    listItem.remove();
+  }
+
+  if (target.className === 'toggle_read_button') {
+    const bookId = target.id.split('-')[1];
+    const readStatusElement = document.querySelector(`#read-${bookId}`);
+    const book = library[bookId];
+    book.toggleRead()
+    const currentStatus = readStatusElement.textContent;
+    readStatusElement.textContent = currentStatus === 'read' ? 'unread' : 'read'
+  }
+});
